9adf5091be1952c3340853cfe13904c911701ad0
jihed krouna <jihedkrouna@gmail.com>
2023-12-23 20:25:44 -0500

    finished first challenge

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,35 @@ fn main() {
     // read first line
     let lines: std::str::Lines<'_> = contents.lines();
     let mut number_final: Vec<i32> = Vec::new();
-    for line in lines.clone().take(1) {
+    for line in lines.clone() {
+        // for line in lines.clone() {
         // let currentline: &str = lines.next().unwrap_or("None");
         let currentline: &str = line;
-        println!("First line: {}", currentline);
-
-        // make first line into a array of character so i can go through it
-        let currentline_chars: Vec<char> = currentline.chars().collect();
+        // println!("line before modification: {}", currentline);
 
         // i have to check in the line if there is "nine, eight, seven, six, five, four, three, two, one, zero"
 
-        let numberstr: Vec<&str> = vec![
-            "nine", "eight", "seven", "six", "five", "four", "three", "two", "one", "zero",
+        let number_tuples: Vec<(&str, usize)> = vec![
+            ("nine", 9),
+            ("eight", 8),
+            ("seven", 7),
+            ("six", 6),
+            ("five", 5),
+            ("four", 4),
+            ("three", 3),
+            ("two", 2),
+            ("one", 1),
+            ("zero", 0),
         ];
-
-        for i in 0..numberstr.len() {
-            if currentline.contains(numberstr[i]) {
-                println!("number in letter: {}", numberstr[i]);
-            }
-        }
-
+        let mut modifiedline: String = String::from(currentline);
+        // for (number_str, number_int) in number_tuples {
+        //     if modifiedline.contains(number_str) {
+        //         // println!("number in letter: {}", number_str);
+        //         modifiedline = modifiedline.replace(number_str, &number_int.to_string());
+        //     }
+        // }
+        let currentline_chars: Vec<char> = modifiedline.chars().collect();
+        println!("\nModified line: {}", modifiedline);
         let mut numbers: Vec<i32> = Vec::new();
         for i in 0..currentline_chars.len() {
             if currentline_chars[i].is_ascii_digit() {
@@ -41,19 +50,25 @@ fn main() {
 
         // keep the first and last number in the array
         if numbers.len() == 1 {
-            number_final.push(numbers[0]);
-            number_final.push(numbers[0]);
+            let mut number_str: String = numbers[0].to_string();
+            number_str.push_str(&numbers[0].to_string());
+            number_final.push(number_str.parse::<i32>().unwrap());
+            // number_final.push(numbers[0].to_string().parse::<i32>().unwrap());
+            // number_final.push(numbers[0]);
+            // number_final.push(numbers[0]);
             // numbers.remove(0);
         } else if numbers.len() >= 2 {
-            number_final.push(numbers[0]);
-            number_final.push(numbers[numbers.len() - 1]);
+            let mut number_str: String = numbers[0].to_string();
+            number_str.push_str(&numbers[numbers.len() - 1].to_string());
+            number_final.push(number_str.parse::<i32>().unwrap());
         } else {
             panic!("Something went wrong at x line");
         }
     }
 
-    // println!("\nNumbers: {:?}", number_final);
+    println!("\nNumbers: {:?}", number_final);
     // give sum of number_final
+
     let mut sum: i32 = 0;
     for i in 0..number_final.len() {
         sum += number_final[i];

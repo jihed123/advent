bec3da08218fb29c5a246412342621ec6ad3436f
jihed krouna <jihedkrouna@gmail.com>
2023-12-23 19:04:18 -0500

    figuring how to use find()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::slice;
 fn main() {
     // read file
     let filepath: &str = "./input1.txt";
@@ -6,26 +7,59 @@ fn main() {
         fs::read_to_string(filepath).expect("Something went wrong reading the file");
 
     // read first line
-    let mut lines: std::str::Lines<'_> = contents.lines();
-    let first_line: &str = lines.next().unwrap_or("None");
-    println!("First line: {}", first_line);
+    let lines: std::str::Lines<'_> = contents.lines();
+    let mut number_final: Vec<i32> = Vec::new();
+    for line in lines.clone().take(1) {
+        // let currentline: &str = lines.next().unwrap_or("None");
+        let currentline: &str = line;
+        println!("First line: {}", currentline);
 
-    // make first line into a array of character so i can go through it
-    let first_line_chars: Vec<char> = first_line.chars().collect();
+        // make first line into a array of character so i can go through it
+        let currentline_chars: Vec<char> = currentline.chars().collect();
 
-    let mut numbers: Vec<i32> = Vec::new();
-    for i in 0..first_line_chars.len() {
-        if first_line_chars[i].is_ascii_digit() {
-            // add it to a array of numbers
-            numbers.push(first_line_chars[i].to_digit(10).unwrap() as i32);
+        // i have to check in the line if there is "nine, eight, seven, six, five, four, three, two, one, zero"
 
-            print!("{} ", first_line_chars[i])
+        let numberstr: Vec<&str> = vec![
+            "nine", "eight", "seven", "six", "five", "four", "three", "two", "one", "zero",
+        ];
+
+        for i in 0..numberstr.len() {
+            if currentline.contains(numberstr[i]) {
+                // println!("{} is in the line", numberstr[i]);
+                let slice: &str = &currentline[currentline.find(numberstr[i]).unwrap()..];
+                println!("Slice: {}", slice);
+            }
+        }
+
+        let mut numbers: Vec<i32> = Vec::new();
+        for i in 0..currentline_chars.len() {
+            if currentline_chars[i].is_ascii_digit() {
+                // add it to a array of numbers
+                numbers.push(currentline_chars[i].to_digit(10).unwrap() as i32);
+
+                print!("{} ", currentline_chars[i])
+            }
+        }
+
+        // keep the first and last number in the array
+        if numbers.len() == 1 {
+            number_final.push(numbers[0]);
+            number_final.push(numbers[0]);
+            // numbers.remove(0);
+        } else if numbers.len() >= 2 {
+            number_final.push(numbers[0]);
+            number_final.push(numbers[numbers.len() - 1]);
+        } else {
+            panic!("Something went wrong at x line");
         }
     }
 
-    // keep the first and last number in the array
-
-    println!("\nNumbers: {:?}", numbers);
-
+    // println!("\nNumbers: {:?}", number_final);
+    // give sum of number_final
+    let mut sum: i32 = 0;
+    for i in 0..number_final.len() {
+        sum += number_final[i];
+    }
+    println!("Sum: {}", sum);
     println!("\nDone!");
 }
